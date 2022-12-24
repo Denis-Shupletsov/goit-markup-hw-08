d2e97f94b7f0314028ddbc685326acb3e4ca7bf4
Denis-Shupletsov <b128k2351763@gmail.com>
2022-12-24 10:28:34 +0200

    stud

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,13 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuContainer = document.querySelector('.menu-container');
+  const openMenuBtn = document.querySelector('.open-menu-button');
+  const closeMenuBtn = document.querySelector('.close-menu-button');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+    menuContainer.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -21,7 +21,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
+    menuContainer.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
